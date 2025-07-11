4b949132a2268025f8318a4a16a130056dcb6744
cl9899 <cl9899@yahoo.com.tw>
2025-07-11 18:43:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "LINE Auto Estimator Bot is Running"
+    return "LINE Auto Estimator Backend OK"
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     body = request.get_data(as_text=True)
     print("Received Webhook:", body)
     return "OK", 200
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
