e0b7f077156e1da8b666180e98857a3e7b0cc2fc
cl9899 <cl9899@yahoo.com.tw>
2025-07-11 19:05:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,3 @@ def webhook():
     body = request.get_data(as_text=True)
     print("Received Webhook:", body)
     return "OK", 200
-
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
