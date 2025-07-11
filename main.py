706843d7cd96b30a515e4bc1586cc361a14d8d60
cl9899 <cl9899@yahoo.com.tw>
2025-07-11 18:31:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-from flask import Flask, request, abort
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "LINE Auto Estimator Bot is running."
+    return "LINE Auto Estimator Bot is Running"
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     body = request.get_data(as_text=True)
     print("Received Webhook:", body)
     return "OK", 200
-
-if __name__ == "__main__":
-    app.run()
