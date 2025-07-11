a6995646e4b33c11298728e613a8aa06f69ecab5
cl9899 <cl9899@yahoo.com.tw>
2025-07-11 22:23:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -43,3 +43,4 @@ def handle_message(event):
 import os
 print("LINE_CHANNEL_SECRET =", os.getenv("LINE_CHANNEL_SECRET"))
 print("SECRET =", os.getenv("LINE_CHANNEL_SECRET"))
+print("LINE_SECRET = ", LINE_CHANNEL_SECRET)
